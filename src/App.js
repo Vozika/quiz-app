af2e7ea7748dda0e42fe5594e264c05d0392c95c
Sergey Vozika <metasoma@yandex.ru>
2022-07-07 13:35:57 +0300

    Let's Do It.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Data from "./data_json.js";
 
 function App() {
+  const [score, setScore] = React.useState(0);
+  const [rightAnswer, setRightAnswer] = React.useState(0);
+  const [wrongAnswer, setWrongAnswer] = React.useState(0);
+
+  const [question, setQuestion] = React.useState({
+    question: "What is the capital of",
+    country: "",
+    options: [],
+  });
+
+  function getRandom(a) {
+    const randomNumber = Math.floor(Math.random() * a);
+    return randomNumber;
+  }
+
+
+
+  function getCountry() {
+    const randomCountry = getRandom(Data.length);
+    const correctCountry = Data[randomCountry];
+    const country = Data[randomCountry].country;
+    correctCountry.isCorrect = true;
+    correctCountry.id = 666;
+    Data.splice(randomCountry, 1);
+
+    const options = [];
+    const splicedCountries = [];
+    options.push(correctCountry);
+
+    for (let i = 0; i < 3; i++) {
+      let countriesLoop = Data;
+      let randomCountryLoop = getRandom(countriesLoop.length);
+      countriesLoop[randomCountryLoop].isCorrect = false;
+      countriesLoop[randomCountryLoop].id = i;
+      options.push(countriesLoop[randomCountryLoop]);
+      splicedCountries.push(countriesLoop[randomCountryLoop]);
+      countriesLoop.splice(randomCountryLoop, 1);
+    }
+
+    splicedCountries.map((country) => {
+      return Data.push(country);
+    });
+    options.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+    setQuestion((prevQuestion) => ({
+      ...prevQuestion,
+      country: country,
+      options: options,
+    }));
+  }
+
+  function optionClicked(isCorrect) {
+    if (isCorrect) {
+      setScore(score + 1);
+      setRightAnswer(rightAnswer + 1);
+    } else {
+      setWrongAnswer(wrongAnswer + 1);
+    }
+    getCountry();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+      {question.country && (
+        <>
+          <h2>Score: {score}</h2>
+          <h3>Right answers: {rightAnswer}</h3>
+          <h3>Wrong answers: {wrongAnswer}</h3>
+        </>
+      )}
+      {question.country && (
+        <h2>
+          {question.question} {question.country}?
+        </h2>
+      )}
+      {question.options.map((answer) => {
+        return (
+          <div
+            onClick={() => optionClicked(answer.isCorrect)}
+            key={answer.id}
+            className="options"
+          >
+            {answer.capital}
+          </div>
+        );
+      })}
+      {!question.country && (
+        <button onClick={getCountry}>
+          {question.country ? "Get New Country" : "Start"}
+        </button>
+      )}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,60 @@
+const Data = [
+    {
+        id: 0,
+        country: "Russia",
+        capital: "Moscow"
+    },
+    {
+        id: 1,
+        country: "Austria",
+        capital: "Vienna"
+    },
+    {
+        id: 2,
+        country: "Bulgaria",
+        capital: "Sofia"
+    },
+    {
+        id: 3,
+        country: "Canada",
+        capital: "Ottawa"
+    },
+    {
+        id: 4,
+        country: "Libya",
+        capital: "Tripoli"
+    },
+    {
+        id: 5,
+        country: "Peru",
+        capital: "Lima"
+    },
+    {
+        id: 6,
+        country: "Tunisia",
+        capital: "Tunis"
+    },
+    {
+        id: 7,
+        country: "Singapore",
+        capital: "Singapore"
+    },
+    {
+        id: 8,
+        country: "Netherlands",
+        capital: "Amsterdam"
+    },
+    {
+        id: 9,
+        country: "Mongolia",
+        capital: "Ulaanbaatar"
+    },
+    {
+        id: 10,
+        country: "Latvia",
+        capital: "Riga"
+    },
+    
+]
+
+export default Data;
